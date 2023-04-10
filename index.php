ab29255a1e6d028f11ea97740aef6c33ff8b0870
jaronavittan <96601217+jaronavittan@users.noreply.github.com>
2023-04-10 22:06:34 +0000

    Initial commit

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+  
+ <head>
+   <link rel="stylesheet" href="styles.css">
+ </head>
+  
+<form>
+  <input type="radio" id="html" name="fav_language" value="HTML">
+  <label for="html">HTML</label><br>
+  <input type="radio" id="css" name="fav_language" value="CSS">
+  <label for="css">CSS</label><br>
+  <input type="radio" id="javascript" name="fav_language" value="JavaScript">
+  <label for="javascript">JavaScript</label> <br>
+  <input type="submit" value="Submit">
+</form>
+</html>
